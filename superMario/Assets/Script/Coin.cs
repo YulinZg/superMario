e030c66d31a88b4a07722009c4ac555a9136e186
OrangestarAlter <lyu19990601@gmail.com>
2022-03-07 20:03:52 +0800

    Coin animation

--- superMario/Assets/Script/Coin.cs
+++ superMario/Assets/Script/Coin.cs
@@ -6,16 +6,42 @@ public class Coin : MonoBehaviour
 {
     public float force;
     public float disappearTime;
+    public Sprite[] sprites;
+    public float interval;
+    private SpriteRenderer spriteRenderer;
+    private float timer = 0;
+    private int index = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
         GetComponent<Rigidbody2D>().AddForce(Vector2.up * force, ForceMode2D.Impulse);
         Invoke(nameof(Disappear), disappearTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sprites.Length != 0)
+            Animate();
     }
 
     void Disappear()
     {
         Destroy(gameObject);
+    }
+
+    void Animate()
+    {
+        timer += Time.deltaTime;
+        if (timer >= interval)
+        {
+            index++;
+            if (index == sprites.Length)
+                index = 0;
+            spriteRenderer.sprite = sprites[index];
+            timer = 0;
+        }
     }
 }
